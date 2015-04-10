83c792f3fb29cf4a2cd696ea1ebda5a439fde6f1
Amon <gosuxm@gmail.com>
2015-04-11 00:52:05 +0800

    no update

--- NewsCamera/NewsCamera/ViewController.m
+++ NewsCamera/NewsCamera/ViewController.m
@@ -17,13 +17,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 - (IBAction)gotoOperate:(id)sender {
     OperateViewController *vc = [[OperateViewController alloc] init];
     [self presentViewController:vc animated:YES completion:nil];
